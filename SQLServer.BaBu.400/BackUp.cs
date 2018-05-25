3edf53ee9b8b80bfed2fe894ab3e58b2474336b3
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2018-05-25 21:03:46 +0200

    Add project files.

---
+++ SQLServer.BaBu.400/BackUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLServer.BaBu
+{
+    public class Backup
+    {
+        /// <summary>
+        /// Restore backup file to database
+        /// </summary>
+        /// <param name="conn">The connection string for the sqlserver database</param>
+        /// <param name="fileName">The full path to the backup file</param>
+        public static void Restore(string conn, string fileName)
+        {
+            var sqlConStrBuilder = new SqlConnectionStringBuilder(conn);
+            var database = sqlConStrBuilder.InitialCatalog;
+            string query = null;
+            using (var connection = new SqlConnection(sqlConStrBuilder.ConnectionString))
+            {
+                query = $"ALTER DATABASE [{database}] SET Single_User WITH Rollback Immediate";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                query = $"USE master RESTORE DATABASE [{database}] FROM DISK='{fileName}' WITH  FILE = 1,  NOUNLOAD,  STATS = 10";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                query = $"USE master ALTER DATABASE [{database}] SET Multi_User";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Take backup from database
+        /// </summary>
+        /// <param name="conn">The connection string for the sqlserver database</param>
+        /// <param name="fileName">The full path to the backup file</param>
+        public static void Take(string conn, string fileName)
+        {
+            var sqlConStrBuilder = new SqlConnectionStringBuilder(conn);
+            using (var connection = new SqlConnection(conn))
+            {
+                var query = $"BACKUP DATABASE [{sqlConStrBuilder.InitialCatalog}] TO DISK='{fileName}'";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ SQLServer.BaBu.400/Bulk.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SQLServer.BaBu
+{
+    public class Bulk
+    {
+        /// <summary>
+        /// Excute SQL Script from string
+        /// </summary>
+        /// <param name="connection">The connection string for the sqlserver database</param>
+        /// <param name="script">The SQL string to excute</param>
+        public static void FromScript(string connection, string script)
+        {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                Server db = new Server(new ServerConnection(conn));
+                string nscript = script.Trim();
+                db.ConnectionContext.ExecuteNonQuery(nscript);
+            }
+        }
+
+        /// <summary>
+        /// Excute SQL Script from file
+        /// </summary>
+        /// <param name="connection">The connection string for the sqlserver database</param>
+        /// <param name="fileName">The full path to the file that have the SQL string to excute</param>
+        public static void FromFile(string connection, string fileName)
+        {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                Server db = new Server(new ServerConnection(conn));
+                string script = File.ReadAllText(fileName).Trim();
+                db.ConnectionContext.ExecuteNonQuery(script);
+            }
+        }
+    }
+}
